57643e7efc622196618ed32b01d46a22e20b0805
David Small <dave@dsmall.co.uk>
2020-10-17 09:47:00 +0100

    fix: Fix nuget build script after folder structure refactor

--- Build/NuGetPush.bat
+++ Build/NuGetPush.bat
@@ -1,6 +1,8 @@
 SET packageVersion=2.0.0-beta01
 
-NuGet.exe pack ../Springboard365.Tools.DynamicsCrm.SolutionVersioner.nuspec -Build -Symbols -Version %packageVersion% -Properties Configuration=Release;id="Springboard365.Tools.DynamicsCrm.SolutionVersioner";author="Springboard 365 Ltd";repo="https://github.com/SpringBoard365/Springboard365.Tools.DynamicsCrm.SolutionVersioner";description="The solution version increment tool for Dynamics Crm.";tags="Solution Version Increment Microsoft Dynamics CRM 2011 2013 2015 2016 SDK XRM 365 Online";
+NuGet.exe pack ../src/SolutionVersioner.nuspec -Build -Symbols -Version %packageVersion% -Properties Configuration=Release;id="Springboard365.Tools.DynamicsCrm.SolutionVersioner";author="Springboard 365 Ltd";repo="https://github.com/SpringBoard365/Springboard365.Tools.DynamicsCrm.SolutionVersioner";description="The solution version increment tool for Dynamics Crm.";tags="Solution Version Increment Microsoft Dynamics CRM 2011 2013 2015 2016 SDK XRM 365 Online";
+
+pause
 
 NuGet.exe push Springboard365.Tools.DynamicsCrm.SolutionVersioner.%packageVersion%.nupkg -Source "https://api.nuget.org/v3/index.json"
 
